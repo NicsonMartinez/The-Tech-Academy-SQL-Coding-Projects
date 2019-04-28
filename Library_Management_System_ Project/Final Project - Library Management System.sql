46136b8368afe62f77ba20462f649db438fa7097
Nicson Martinez <martineznicson@gmail.com>
2019-04-28 13:38:21 -0700

    Update Library SQL Project
    I'm continuing to add data on the tables

--- Library_Management_System_ Project/Final Project - Library Management System.sql
+++ Library_Management_System_ Project/Final Project - Library Management System.sql
@@ -60,6 +60,7 @@ CREATE TABLE tbl_book_loans (
 	('Bloomingdale', '150 West 100th Street, NYC'),
 	('Chatham Square', '33 East Broadway, NYC')
 	('Sharpstown', '1 Main St. Portland, OR')
+	('Central', 'Alberta St. Portland,OR')
 ;
 
 INSERT INTO tbl_books
@@ -68,7 +69,7 @@ INSERT INTO tbl_books
 	('The Hunger Games', 'Scholastic'),
 	('Harry Potter and the Order of the Phoenix', 'Scholastic'),
 	('To Kill a Mockingbird', 'HarperCollins'),
-	('Pride and Prejudice', 'Modern Library'),
+	('Pride and Prejudice', 'Modem Library'),
 	('Twilight', 'Little, Brown and Company'),
 	('The Book Thief', 'Alfred A. Knopf'),
 	('The Chronicles of Narnia', 'HarperCollins'),
@@ -77,15 +78,18 @@ INSERT INTO tbl_books
 	('Gone with the Wind', 'Warner Books'),
 	('The Fault in Our Stars', 'Dutton Books'),
 	('The Giving Tree', 'HarperCollins'),
-	('The Hitchhiker''s Guide to the Galaxy', ''),
-	('', ''),
-	('', ''),
-	('', ''),
-	('', ''),
-	('', ''),
-	('', ''),
-	('', ''),
-	('', ''),
+	('The Hitchhiker''s Guide to the Galaxy', 'Del Rey'),
+	('Wuthering Heights', 'Norton'),
+	('The Da Vinci Code', 'Anchor'),
+	('Memoirs of a Geisha', 'Penguin Random House'),
+	('Alice''s Adventures in Wonderland & Through the Looking-Glass', 'Penguin Random House'),
+	('The Picture of Dorian Gray', 'Modem Library'),
+	('Les Misérables', 'Signet Classics'),
+	('Jane Eyre', 'Penguin Random House'),
+	('The Lost Tribe', 'Carpet Bombing Culture'),
+	('Divergent', 'HarperCollins'),
+	('It', 'Viking Press'),
+	('Pet Sematary', 'Doubleday')
 ;
 
 INSERT INTO tbl_authors
@@ -103,16 +107,18 @@ INSERT INTO tbl_authors
 	(110, 'Margaret Mitchell'),
 	(111, 'John Green'),
 	(112, 'Shel Silverstein'),
-	(, ''),
-	(, ''),
-	(, ''),
-	(, ''),
-	(, ''),
-	(, ''),
-	(, ''),
-	(, ''),
-	(, ''),
-	(, ''),
+	(113, 'Douglas Adams'),
+	(114, 'Emily Brontë,  Richard J. Dunn '),
+	(115, 'Dan Brown'),
+	(116, 'Arthur Golden'),
+	(117, 'Lewis Carroll'),
+	(118, 'Oscar Wilde'),
+	(119, 'Victor Hugo'),
+	(120, 'Charlotte Brontë'),
+	(121, 'Mark W. Lee'),
+	(122, 'Veronica Roth'),
+	(123, 'Stephen King'),
+	(124, 'Stephen King')
 ;
 
 INSERT INTO tbl_book_authors
@@ -128,11 +134,20 @@ INSERT INTO tbl_book_authors
 	(108, 4, 3),
 	(109, 5, 8),
 	(110, 5, 4),
-	(111, 1, 9),
-	(112, 1, 6),
-	(, , ),
-	(, , ),
-	(, , ),
+	(111, 6, 9),
+	(112, 6, 6),
+	(113, 1, 10),
+	(114, 1, 2),
+	(115, 2, 5),
+	(116, 2, 7),
+	(117, 3, 1),
+	(118, 3, 3),
+	(119, 4, 9),
+	(120, 4, 11),
+	(121, 5, 5),
+	(122, 5, 3),
+	(123, 6, 8),
+	(124, 6, 10)
 ;
 
 INSERT INTO tbl_publisher
@@ -140,26 +155,19 @@ INSERT INTO tbl_publisher
 	VALUES
 	('Scholastic', '557 Broadway, New York City, New York, U.S. 10012', '800-724-6527'),
 	('HarperCollins', '195 Broadway, New York, NY 10007', '212-207-7000'),
-	('Modern Library', 'New York, New York', 'N/A'),
+	('Modem Library', 'New York, New York', 'N/A'),
 	('Little, Brown and Company', '3 Center Plz Ste 100 Boston, MA 02108', '617-227-0730'),
 	('Alfred A. Knopf', '1745 Broadway New York, NY 10019', 'N/A'),
 	('NAL', 'New York, New York', 'N/A'),
 	('Ballantine Books', 'New York, New York', 'N/A'),
 	('Warner Books', 'New York, New York', 'N/A'),
 	('Dutton Books', 'New York, New York', 'N/A'),
-	('', '', ''),
-	('', '', ''),
-	('', '', ''),
-	('', '', ''),
-	('', '', ''),
-	('', '', ''),
-	('', '', ''),
-	('', '', ''),
-	('', '', ''),
-	('', '', ''),
-	('', '', ''),
-	('', '', ''),
-	('', '', ''),
-	('', '', ''),
-	('', '', ''),
-	('', '', ''),
+	('Del Rey', 'New York, New York', 'N/A'),
+	('Norton', '500 5th Ave # 6, New York, NY 10110', '212-354-5500'),
+	('Anchor', '1745 Broadway New York, NY 10019', '212-782-9000'),
+	('Penguin Random House', 'New York, New York', 'N/A'),
+	('Signet Classics', 'New York, New York', 'N/A'),
+	('Carpet Bombing Culture', 'United Kingdom', 'N/A'),
+	('Viking Press', 'New York, New York', 'N/A'),
+	('Doubleday', '1745 Broadway New York, NY 10019', 'N/A')
+;
